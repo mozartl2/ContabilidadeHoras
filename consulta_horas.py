c8073fe462ef8ce5aebd8702a3e6583bf1738103
mozartl2 <lucas.mozsart@gmail.com>
2025-04-11 15:41:35 -0300

    Novas funcionalidades

--- consulta_horas.py
+++ consulta_horas.py
@@ -2,10 +2,21 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-def valida_data(data_str):
+class DiaSob:
+    def __init__(self, dia, horas_total, feriado):
+        self.dia = dia
+        self.horas_total = horas_total
+        self.horasSob = None
+        self.atendimentos = []
+        self.feriado = feriado
+        self.valor50 = None
+        self.valor75 = None
+        self.valor100 = None
+
+def valida_data(data_str: str) -> bool:
     try:
-        datetime.strptime(data_str, "%d/%m/%Y")
-        return True
+        data = datetime.strptime(data_str, "%d/%m/%Y")
+        return data.weekday() == 0 
     except ValueError:
         return False
 
@@ -28,11 +39,8 @@ def consulta_chamados(data_inicial_dt, data_final_dt):
 
     lista_chamados = requests.get(url, headers=headers)
     chamados_json = lista_chamados.json()
-    if len(chamados_json['data']) != 0:
+    if len(chamados_json['data']) != 0 and chamados_json['data']:
         return chamados_json
-    else: 
-        return False
-
 
 def consulta_atendente():
     pass
@@ -41,14 +49,35 @@ def consulta_atendente():
 with open("request_data.json", "r", encoding="utf-8") as arquivo:
     dadosJson = json.load(arquivo) 
 
-data_inicial = input("Qual a data inicial que deseja consulta o periodo de sobre aviso? lembre de colocar sempre uma segunda-feira(Formato: DD/MM/YYYY)")
-if not valida_data(data_inicial):
-    print("Data inválida ou inexistente, digite novamente")
+#Input da data incial que vai ser usado para busca
+data_inicial = input("Qual a data inicial que deseja consulta o periodo de sobre aviso? lembre de colocar sempre uma segunda-feira(Formato: DD/MM/YYYY)\n")
+while not valida_data(data_inicial):
+    data_inicial = input("Data inválida ou inexistente, digite novamente\n")
+
+data_inicial_obj = datetime.strptime(data_inicial, "%d/%m/%Y")
+data_final_obj = data_inicial_obj + timedelta(days=7)
+
+dias_semana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+datas_atendimento  = [data_inicial_obj + timedelta(days=i) for i in range(7)]
+
+semana_atendimento = {}
+
+for i in range(7):
+    if 0 <= i < 5:
+        total_horas = 3
+        feriado = False
+    elif i < 6: 
+        total_horas = 10
+        feriado = False       
+    else:
+        total_horas = 8
+        feriado = True
+    string_data_iteracao = datetime.strftime(datas_atendimento[i], "%d/%m/%Y")
+    semana_atendimento[string_data_iteracao] = DiaSob(dias_semana[i],total_horas, feriado)
+
+chamados_dic = consulta_chamados(data_inicial_obj, data_final_obj)
+if chamados_dic:
+    for chamado in chamados_dic:
+        print(chamado)
 else:
-    data_inicial_obj = datetime.strptime(data_inicial, "%d/%m/%Y")
-    data_final_obj = data_inicial_obj + timedelta(days=7)
-
-    chamados_dic = consulta_chamados(data_inicial_obj, data_final_obj)
-    jsonformat = json.dumps(chamados_dic, indent=4, ensure_ascii="utf-8")
-    print(jsonformat)
-
+    print('Não foram encontrados chamados no periodo especificado')
